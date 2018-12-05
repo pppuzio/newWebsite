466d83a74dd74d790037389e05cb7240f013e5f6
pppuzio <pppuzio@gmail.com>
2018-12-05 19:01:09 +0100

    header progress/ added hamburger menu

--- js/app.js
+++ js/app.js
@@ -15,8 +15,30 @@ $(function () {
             $('.fullscreen-height').css('height',window.innerHeight+'px');
         })
     }
-
     fullscreenSize();
+
+    function hamburgerToggler(){
+        $(".hamburger-toggler--wrapper").click(function(){
+            $(this).toggleClass('open');
+            menuSlideToggle();
+        })
+    }
+    hamburgerToggler();
+
+    function menuSlideToggle(){
+
+        var mainMenu = $('.main-menu');
+        var hamburgerToggler = $('.hamburger-toggler--wrapper');
+
+        console.log(mainMenu,hamburgerToggler);
+
+        if (hamburgerToggler.hasClass("open")){
+            mainMenu.css("top","0");
+        }else {
+            mainMenu.css('top','-100%');
+        }
+
+    }
 
 
 
